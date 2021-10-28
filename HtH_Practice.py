7fb76e6170c9d08db9a3ebef34ef1b760d32a807
michhy01 <75771552+michhy01@users.noreply.github.com>
2021-10-27 18:13:13 -0700

    python exercises

---
+++ HtH_Practice.py
@@ -0,0 +1,66 @@
+print ("Hello world")
+my_text="I like pizza"
+print(my_text)
+glass_of_water=3
+print("I drink", glass_of_water, "glasses of water today.")
+glass_of_water=3
+glass_of_water=glass_of_water + 1
+print()
+print("I drink", glass_of_water)
+men_stepped_on_the_moon=5
+print(men_stepped_on_the_moon, "men steeped on the moon")
+String1= 'My reason for coding is a better life and understanding of tech'
+print("why do I code?:")
+print(String1)
+print(type(String1))
+global_mean_sea_level_2018=21
+float(global_mean_sea_level_2018)
+print(float(global_mean_sea_level_2018))
+staying_alive="Pizza is the best"
+print(bool(staying_alive))
+men_stepped_on_the_moon=12
+answer_1=men_stepped_on_the_moon
+print(type(answer_1))
+my_reason_for_coding="intergalactic travel"
+answer_2=my_reason_for_coding
+print(type(answer_2))
+global_mean_sea_level_delta_2018=21.36
+answer_3=global_mean_sea_level_delta_2018
+print(type(answer_3))
+staying_alive=True
+answer_4=staying_alive
+print(type(answer_4))
+print()
+my_grade="10"
+answer_5=int(my_grade)
+print(type(answer_5))
+my_temp=97.70
+answer_6=int(my_temp)
+print(type(answer_6))
+shoe_price="69.99"
+answer_7=float(shoe_price)
+print(type(answer_7))
+gross_world_product=84.84
+gwp_str=str(gross_world_product)
+answer_8="In 2018 gross product of the world (GWP) was " + gwp_str + "in trillion US dollar."
+print(answer_8)
+print(type(answer_8))
+print()
+gift_list=[]
+answer_9=gift_list
+print(type(answer_9))
+grocery_items={}
+answer_10=grocery_items
+print(type(answer_10))
+bucket_list= ()
+answer_11=bucket_list
+print(type(answer_11))
+gift_list=["strawberry", "bananas", "apples"]
+print(gift_list)
+print(type(gift_list))
+grocery_list={"fruit": "bananas,and green apples", "drinks": "orange juice" }
+print(grocery_list)
+print(type(grocery_list))
+bucket_list=("traveling to 5 different countries", "go to MCR concert", "skydiving")
+print(bucket_list)
+print(type(bucket_list))

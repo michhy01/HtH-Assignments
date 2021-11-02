cf0d629cc74e6116606ca81985a046c4e2257921
michhy01 <75771552+michhy01@users.noreply.github.com>
2021-11-02 16:31:31 -0700

    practice with lists

---
+++ Lists_practice.py
@@ -0,0 +1,31 @@
+cities=["Oakland", "Atlanta", "New York City", "Seattle", "Memphis", "Miami", "Boston", "Los Angeles", "Denver", "New Orleans"]
+print(cities)
+print(type(cities))
+colors=["Orange", "Blue", "Yellow", "Pink", "Red", "Purple", "Brown", "Black", "White", "Green"]
+print(colors)
+print(cities[-3])
+print(cities[2])
+print(cities[0])
+print()
+print(colors[-10])
+print(colors[-1])
+print(colors[4])
+print()
+print(cities[0:4])
+top_cities=cities[0:4]
+print(top_cities)
+print()
+cities[0]="San Francisco"
+cities[2]="Brooklyn"
+cities[-3]="Hollywood"
+cities[5]="Tampa"
+print(cities)
+cities.append("Oakland")
+cities.extend(["New York City", "Los Angeles"])
+cities.insert(0,"Miami")
+print(cities)
+print()
+del cities[4]
+cities.pop(-4)
+cities.remove("Memphis")
+print(cities)

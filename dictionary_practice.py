ecc1507614d30e83e39f96302ed91b0c0b98a7b5
michhy01 <75771552+michhy01@users.noreply.github.com>
2021-11-02 15:55:03 -0700

    Practice with Dictionaries

---
+++ dictionary_practice.py
@@ -0,0 +1,38 @@
+food_prices={"Chicken": 1.59, "Beef": 1.99, "Cheese": 1.00, "Milk": 2.50}
+print(food_prices)
+print(type(food_prices))
+print()
+people_age={"Luna": 20, "Kitty": 25, "Mauricio": 4, "Rowena": 16}
+print(people_age)
+
+print(food_prices["Chicken"])
+print(food_prices["Beef"])
+print(food_prices["Cheese"])
+print(food_prices["Milk"])
+print()
+print(people_age["Luna"])
+print(people_age["Kitty"])
+print(people_age["Mauricio"])
+print(people_age["Rowena"])
+print()
+shoes_instore={"Jordan 13": 1, "Yeezy": 8, "Foamposite": 10, "Air Max": 5, "SB Dunk": 20}
+print(shoes_instore)
+print(shoes_instore["SB Dunk"] - 2)
+print(shoes_instore["Yeezy"] + 1)
+print(shoes_instore["Jordan 13"] + 7)
+print(shoes_instore["Yeezy"] + 7)
+print(shoes_instore["Foamposite"] + 7)
+print(shoes_instore["Air Max"] + 7)
+print(shoes_instore["SB Dunk"] + 7)
+print(shoes_instore["Jordan 13"] -3)
+print(shoes_instore["Yeezy"] - 3)
+print(shoes_instore["Foamposite"] - 3)
+print(shoes_instore["Air Max"] - 3)
+print(shoes_instore["SB Dunk"] -3)
+shoes_instore["Vans"]=13
+shoes_instore["Converse"]=25
+shoes_instore["Nike"]=5
+print(shoes_instore)
+del shoes_instore["Nike"]
+del shoes_instore["Yeezy"]
+print(shoes_instore)

70960999c1b3e8ba803da0dc7993b418417f5d02
michhy01 <75771552+michhy01@users.noreply.github.com>
2021-11-08 14:12:44 -0800

    updated lists practice

--- Lists_practice.py
+++ Lists_practice.py
@@ -33,3 +33,35 @@ del cities[4]
 cities.pop(-4)
 cities.remove("Memphis")
 print(cities)
+
+#step IX
+def all_printed_cities(list):
+    for el in list:
+        print(el)
+    return "All cities are printed"
+
+
+#Step X
+
+def reorganized_list(list):
+    print(list)
+    counter = 0 
+
+    while counter < len(list):
+        item1 = list[counter]
+        item2 = list[counter - 1]
+
+        if len(item1) >= len(item2):
+            counter += 1
+            continue
+        elif counter + 1 == len(list) - 1:
+            break
+        else:
+            list.remove(item1)
+            list.append(item1)
+            counter += 1
+
+    return list
+
+print(all_printed_cities(cities))
+print(reorganized_list(cities))
